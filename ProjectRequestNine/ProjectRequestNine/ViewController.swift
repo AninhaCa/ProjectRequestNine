f18abc8fe712e8466167e0c22d4578406f8c9a09
Aninha <aninhapss2000@gmail.com>
2023-03-20 22:22:50 -0300

    Added pods

--- ProjectRequestNine/ProjectRequestNine/ViewController.swift
+++ ProjectRequestNine/ProjectRequestNine/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var arrayPizza: [PizzaElement]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
 
+    func setupTableView() {
+        requestPizza()
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MyCustomCellXIB", bundle: nil), forCellReuseIdentifier: "cellXIB")
+    }
 
+    func requestPizza() {
+        AF.request("https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/pizza", method: .get).response { response in
+            let pizza = try? JSONDecoder().decode([PizzaElement].self, from: response.data ?? Data())
+            self.arrayPizza = pizza
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayPizza?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cellXIB", for: indexPath) as? MyCustomCellXIB {
+            cell.setupXIB(pizzaElement: arrayPizza?[indexPath.row])
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
 }
 
